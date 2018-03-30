2c6fa748d839d7fc2b3944a3ca686b23b4d6a623
mathieuvachon2 <vmat96@gmail.com>
2018-03-30 17:44:11 -0400

    modularized

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,9 +1,20 @@
 import pywin
 import pyttsx
+from pytesseract import image_to_string
+from PIL import Image
+import os
+from cv2 import *
+import image_to_text as iit
 
 engine = pyttsx .init()
 # Opening text file
-testFile = open("test.txt", "r+")
+# initialize the camera
+cam = VideoCapture(1)   # 1 -> index of camera
+s, img = cam.read()
+
+iit.func()
+
+testFile = open("out.txt", "r+")
 for line in testFile.readlines():
     engine.say(line)
     engine.runAndWait()
