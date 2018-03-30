1f501d641d7ea69173207af9edde42b4f1ab1719
mathieuvachon2 <vmat96@gmail.com>
2018-03-30 18:46:03 -0400

    Two halves cropped

--- image_to_text.py
+++ image_to_text.py
@@ -1,6 +1,7 @@
 from pytesseract import image_to_string
 from PIL import Image
 import os
+import numpy as np
 from cv2 import *
 
 def func():
@@ -16,7 +17,28 @@ def func():
         imwrite("Image.jpg",img) #save image
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    img = cv2.imread('Image.jpg')
+    h = np.size(img, 0)
+    w = np.size(img, 1)
+
     x = raw_input('Do you want to carry on ?')
     if (x == "n"): exit()
 
-    os.system("tesseract Image.jpg out") # Tesseract needs to be installed
+    # Cropping the left and right halves of the picture
+    image = Image.open("Image.jpg")
+    cnt = 0
+    while(True) :
+
+        if (cnt == 2) : break # Both pages have been cropped
+
+        if (cnt == 0) : # Crop the left half
+            image2 = image.crop((0, 0, w/2, h))
+            image2.save("Image2.jpg")
+            os.system("tesseract Image2.jpg out1")
+            cnt = cnt + 1
+
+        if (cnt == 1) : # Crop the right half
+            image2 = image.crop((w/2, 0, w, h))
+            image2.save("Image2.jpg")
+            os.system("tesseract Image2.jpg out2")
+            cnt = cnt + 1
