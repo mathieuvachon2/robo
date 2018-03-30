07de49eece908c32d2143c6de3fe069255ed9818
mathieuvachon2 <vmat96@gmail.com>
2018-03-30 17:42:50 -0400

    modularized

--- image_to_text.py
+++ image_to_text.py
@@ -1,20 +1,22 @@
 from pytesseract import image_to_string
 from PIL import Image
+import os
 from cv2 import *
-# initialize the camera
-cam = VideoCapture(1)   # 1 -> index of camera
-s, img = cam.read()
 
-if s:    # frame captured without any errors
-    namedWindow("cam-test",WINDOW_NORMAL)
-    imshow("cam-test",img)
-    waitKey(0)
-    destroyWindow("cam-test")
-    imwrite("Image.jpg",img) #save image
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def func():
+    # initialize the camera
+    cam = VideoCapture(1)   # 1 -> index of camera
+    s, img = cam.read()
 
-# load the image as a PIL/Pillow image, apply OCR, and then delete
-# the temporary file
-imag = Image.open("Image.jpg")
-text = image_to_string(imag)
-print(text)
+    if s:    # frame captured without any errors
+        namedWindow("cam-test",WINDOW_NORMAL)
+        imshow("cam-test",img)
+        waitKey(0)
+        destroyWindow("cam-test")
+        imwrite("Image.jpg",img) #save image
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    x = raw_input('Do you want to carry on ?')
+    if (x == "n"): exit()
+
+    os.system("tesseract Image.jpg out") # Tesseract needs to be installed
