213a81485dea14fcfe3f578fe115a1c0c63b7891
Simon Zheng <simz97@outlook.com>
2018-03-31 00:00:50 -0400

    Stuff

--- Page_flipper_code/Page_flipper_code.ino
+++ Page_flipper_code/Page_flipper_code.ino
@@ -4,8 +4,6 @@
 #define MINQUARTDELAY 50
 #define MINHALFDELAY 200
 
-Servo getServo() { return servo; }
-
 void turn(Servo servo, int deg, int ms) {
   servo.write(deg);
   delay(ms);
@@ -37,12 +35,10 @@ void sweep(Servo servo) {
   }
 }
 
-Servo servo0; //the wheel
-Servo servo1; //the arm
+Servo servo0;
+Servo servo1;
 int sensorvalue0;
 int sensorvalue1;
-int posArm;
-int posWheel;
 
 ////get output from python program: positive count if hit, negative or 0 if hold
 //// int count = get.countFromProgram;
@@ -50,65 +46,28 @@ int posWheel;
 //int count1 = -1;
 //int count2 = 0;
 
-void setup(){
-  //pinMode(A0, INPUT);
-  pinMode(2, OUTPUT);
-  servo0.attach(2); // servo controlling wheel
+void setup()
+{
+  pinMode(A0, INPUT);
+  pinMode(9, OUTPUT);
+  servo0.attach(9);
 
-  //pinMode(A1, INPUT);
+  pinMode(A1, INPUT);
   pinMode(8, OUTPUT);
-  servo1.attach(8); // servo controlling arm
-
-  for(posArm=30;posArm >=0;posArm--){
-      servo1.write(posArm); // servo on pin 9 connected to wheel returns to start position
-      delay(15);
-  }
+  servo1.attach(8);
 
   for (int i = 0; i < 7; i++) { // do this cycle 7? times to ensure paper gets discharged
-    /*servo0.write(0); // adjust degrees for proper alignment with page
-    delay(15);
+    servo0.write(0);
+    delay(1000);
     servo0.write(180); // servo on pin 9 connected to wheel rotates 180 degrees
-    delay(1500);
+    delay(1000);
     servo1.write(0); // servo on pin 8 connected to arm rotates 180 degrees to lift up
-    delay(1500);
-    servo1.write(40);
-    delay(1500);
+    delay(1000);
+    servo1.write(180);
+    delay(1000);
     servo0.write(0); // servo on pin 9 connected to wheel returns to start position
-    delay(1500);
+    delay(1000);
     servo1.write(0); // servo on pin 8 connected to arm returns to bottom start position
-    // repeat 7? times  */
-    for(posArm=0;posArm <=30;posArm++){
-      servo1.write(posArm); // servo on pin 9 connected to wheel returns to start position
-      delay(15);
-    }
-    for(posWheel=0;posWheel <= 180;posWheel++){
-      servo0.write(posWheel); // servo on pin 9 connected to wheel returns to start position
-      delay(5);
-    }
-    for(posArm=30;posArm >= 0;posArm--){
-      servo1.write(posArm); // servo on pin 9 connected to wheel returns to start position
-      delay(15);
-    }
-    for(posWheel=180;posWheel >= 0;posWheel--){
-      servo0.write(posWheel); // servo on pin 9 connected to wheel returns to start position
-      delay(5);
-    }
-    for(posArm=0;posArm <=30;posArm++){
-      servo1.write(posArm); // servo on pin 9 connected to wheel returns to start position
-      delay(15);
-    }
-    for(posWheel=0;posWheel <= 180;posWheel++){
-      servo0.write(posWheel); // servo on pin 9 connected to wheel returns to start position
-      delay(5);
-    }
-    for(posArm=30;posArm >= 0;posArm--){
-      servo1.write(posArm); // servo on pin 9 connected to wheel returns to start position
-      delay(15);
-    }
-    for(posWheel=180;posWheel >= 0;posWheel--){
-      servo0.write(posWheel); // servo on pin 9 connected to wheel returns to start position
-      delay(5);
-    }
     // repeat 7? times  
   }
 
